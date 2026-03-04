460af42c7d1dc37a1325dc74dc2d85f2a938f75b
Islam <theorem037@gmail.com>
2026-03-04 21:52:53 +0200

    Math method

--- math.js
+++ math.js
@@ -1,3 +1,9 @@
 console.log("Hello, Math!");
-
+Math.sqrt(16); // Returns the square root of 16
+Math.pow(2, 3); // Returns 2 raised to the power of 3
+Math.random(); // Returns a random number between 0 (inclusive) and 1 (exclusive)
+Math.PI; // Returns the value of π (pi)
+Math.round(4.7); // Rounds 4.7 to the nearest integer (5)
+Math.floor(4.7); // Rounds 4.7 down to the nearest integer (4)
+Math.ceil(4.3); // Rounds 4.3 up to the nearest integer (5)         
 // Math is a built-in object in JavaScript
